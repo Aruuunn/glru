54a01225680ca717b5d8b91c26bfadfd0432c0ae
Arun Murugan <arunmurugan200@gmail.com>
2021-08-01 19:57:32 +0530

    feat: implemented LRU with no max capacity

--- glru.go
+++ glru.go
@@ -1,21 +1,48 @@
 package glru
 
+import (
+	"errors"
 
-type Glru struct {}
+	"github.com/ArunMurugan78/glru/dll"
+)
 
+type Glru struct {
+	nodeMap  map[string]*dll.Node
+	maxItems int
+	list     *dll.Dll
+}
 
 type Config struct {
-  MaxItems int
+	MaxItems int
 }
 
+var ErrKeyNotFound = errors.New("key not found")
 
 func New(config Config) *Glru {
-  return &Glru{}
+  return &Glru{maxItems: config.MaxItems, list: dll.New(), nodeMap: make(map[string]*dll.Node)}
 }
 
-func (cache *Glru) Set(key string, value interface{}) {}
+func (cache *Glru) Set(key string, value interface{}) {
+	node, ok := cache.nodeMap[key]
 
+	if ok {
+		if node.Value != value {
+			node.Value = value
+		}
 
-func (cache *Glru) Get(key string) interface{} {
-  return nil
+		return
+	}
+
+	cache.nodeMap[key] = cache.list.Prepend(value)
+}
+
+func (cache *Glru) Get(key string) (interface{}, error) {
+	node, ok := cache.nodeMap[key]
+
+	if ok {
+		// Brings the accessed node to the front of the list in O(1) time complexity
+		cache.list.DeleteAndInsertAtHead(node)
+		return node.Value, nil
+	}
+	return nil, ErrKeyNotFound
 }
