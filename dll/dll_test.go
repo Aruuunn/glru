5a23f36dffd66326472bc7c4446693984738b139
Arun Murugan <arunmurugan200@gmail.com>
2021-08-01 19:54:26 +0530

    test: DeleteNode method

--- dll/dll_test.go
+++ dll/dll_test.go
@@ -21,13 +21,39 @@ func TestPrepend(t *testing.T) {
 	assertAfterPrepend(t, []interface{}{"One", 2, "three", 6, 1.2})
 }
 
-func assertAfterPrepend(t *testing.T, values []interface{}) {
+func TestDeleteNode(t *testing.T) {
+	assertAfterDeletion(t, 0, []interface{}{"One"})
+	assertAfterDeletion(t, 1, []interface{}{1, 2, 3, 4})
+	assertAfterDeletion(t, 3, []interface{}{1, 2, 3, 4})
+
+	assertAfterDeletion(t, 0, []interface{}{1, 2, 3, 4})
+}
+
+func assertAfterDeletion(t *testing.T, index int, values []interface{}) {
+	l := getListAfterPrepending(values)
+
+	idx := 0
+	ptr := l.GetHead()
+	for idx < index {
+		ptr = ptr.Next
+		idx++
+	}
+	l.DeleteNode(ptr)
+	assert.Equal(t, true, traverseAndCheckValue(l.GetHead(), remove(values, index)))
+}
+
+func getListAfterPrepending(values []interface{}) *dll.Dll {
 	l := dll.New()
 
 	for _, value := range values {
 		l.Prepend(value)
 	}
 
+	return l
+}
+
+func assertAfterPrepend(t *testing.T, values []interface{}) {
+	l := getListAfterPrepending(values)
 	assert.Equal(t, true, traverseAndCheckValue(l.GetHead(), values))
 }
 
@@ -67,4 +93,8 @@ func TestTestUtil(t *testing.T) {
 	head.Next.Prev = head
 
 	assert.Equal(t, true, traverseAndCheckValue(head, []interface{}{"Two", "One"}))
+}
+
+func remove(slice []interface{}, s int) []interface{} {
+	return append(slice[:(len(slice)-s-1)], slice[(len(slice)-s-1)+1:]...)
 }
